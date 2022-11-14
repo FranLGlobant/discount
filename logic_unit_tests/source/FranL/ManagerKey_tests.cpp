76a18a6dfc1fc0f46b628542962edcede6b2fc00
francisco.luque <francisco.luque@globant.com>
2022-11-14 12:29:53 -0300

    Add Tests for Manager, not working

---
+++ logic_unit_tests/source/FranL/ManagerKey_tests.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+
+#include "ManagerKey.h"
+
+namespace dsc
+{
+	namespace tests
+	{
+s		static std::string kTestString("testString");
+
+		static std::string kTestKeyGetter(std::string const& input)
+		{
+			return input;
+		};
+		
+		TEST(ManagerKey, ManagerKey_get)
+		{
+			dsc::ManagerKey<std::string> no(kTestKeyGetter);
+			EXPECT_EQ(no.add(std::move(kTestString)), true);
+
+			ASSERT_EQ(no.get(kTestString), kTestString);
+		}
+		
+		
+
+	} // namespace tests
+} // namespace dsc
---
+++ logic_unit_tests/source/FranL/Manager_tests.cpp
@@ -0,0 +1,91 @@
+#include "gtest/gtest.h"
+
+#include "Manager.h"
+
+namespace dsc
+{
+	namespace tests
+	{
+		static std::list<std::string> const kTestList( {"test1", "test2", "banana", "apple", "toucan", "penguin", "hummingbird" });
+		static std::string kTestString("testString");
+
+		static bool isTestString(std::string& const input)
+		{
+			return input == kTestString;
+		};
+		
+		TEST(Manager, Manager_objects)
+		{
+			dsc::Manager<std::string> no;
+			ASSERT_EQ(no.objects().empty(), true);
+		}
+		
+		TEST(Manager, Manager_add)
+		{
+			dsc::Manager<std::string> no;
+			
+			EXPECT_EQ(no.add(std::move(kTestString)), true);
+			ASSERT_EQ(no.objects(), { kTestString });
+		}
+		TEST(Manager, Manager_removeByValue)
+		{
+			dsc::Manager<std::string> no;
+
+			EXPECT_EQ(no.add(std::move(kTestString)), true);
+			EXPECT_EQ(no.objects(), { kTestString });
+
+			EXPECT_EQ(no.remove(kTestString), true);
+			ASSERT_EQ(no.objects(), { });
+		}
+
+		TEST(Manager, Manager_removeByRef)
+		{
+			dsc::Manager<std::string> no;
+
+			EXPECT_EQ(no.add(kTestSstd::move(kTestString)tring), true);
+			EXPECT_EQ(no.objects(), { kTestString });
+
+			EXPECT_EQ(no.remove(0), true);
+			ASSERT_EQ(no.objects().empty(), true);
+		}
+
+		TEST(Manager, Manager_removeAll)
+		{
+			dsc::Manager<std::string> no;
+
+			EXPECT_EQ(no.add(std::move(kTestString)), true);
+			EXPECT_EQ(no.objects(), {kTestString});
+
+			no.removeAll();
+			ASSERT_EQ(no.objects().empty(), true);
+		}
+
+		TEST(Manager, Manager_find)
+		{
+			dsc::Manager<std::string> no;
+
+			EXPECT_EQ(no.add(std::move(kTestString)), true);
+			EXPECT_EQ(no.objects(), { kTestString });
+			ASSERT_EQ(no.find(dsc::tests::isTestString), kTestString);
+		}
+
+		TEST(Manager, Manager_at)
+		{
+			dsc::Manager<std::string> no;
+
+			EXPECT_EQ(no.add(std::move(kTestString)), true);
+			EXPECT_EQ(no.objects(), { kTestString });
+			ASSERT_EQ(no.at(0), kTestString);
+		}
+
+		TEST(Manager, Manager_random)
+		{
+			dsc::Manager<std::string> no;
+
+			EXPECT_EQ(no.add(std::move(kTestString)), true);
+			EXPECT_EQ(no.objects(), { kTestString });
+			ASSERT_EQ(no.random(), kTestString);
+		}
+
+	} // namespace tests
+} // namespace dsc
